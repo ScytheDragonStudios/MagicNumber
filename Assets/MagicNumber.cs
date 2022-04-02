cecc5cc75c718e870f5c0d548d159e3a0b3a8e93
ScytheDragonStudios <sdstudios@yahoo.com>
2022-04-02 12:34:27 -0400

    2nd Commit, added debug log for console reading and some variable fixes

--- Assets/MagicNumber.cs
+++ Assets/MagicNumber.cs
@@ -6,16 +6,30 @@ public class MagicNumber : MonoBehaviour
 {
 
     //Variables
-    int maxNum = 1000;
-    int minNum = 1;
-    int guess = 500;
     int max;
     int min;
-    int guesses;
+    int guess;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Welcome to Magic Number!");
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Welcome . . . to the Magic Number!");
+        Debug.Log("Do you believe that a mere program such as I can read your mind?");
+        Debug.Log("I'm going to prove to you that I can!");
+        Debug.Log("Pick a number between " + max + " and " + min + "!");
+        Debug.Log("Don't tell me what it is though! That's the fun!");
+        Debug.Log("Is your number Higher or lower than: " + guess + "?");
+        Debug.Log("Push up = Higher, Push down = Lower, Push Enter = Correct");
+        max = max + 1;
     }
 
     // Update is called once per frame
