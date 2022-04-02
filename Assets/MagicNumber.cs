9609d81172f0aa7f329c9ee606184f6b5304caf9
ScytheDragonStudios <sdstudios@yahoo.com>
2022-04-02 12:24:53 -0400

    First new commit

---
+++ Assets/MagicNumber.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicNumber : MonoBehaviour
+{
+
+    //Variables
+    int maxNum = 1000;
+    int minNum = 1;
+    int guess = 500;
+    int max;
+    int min;
+    int guesses;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Magic Number!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
